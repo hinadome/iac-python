dee74d78e64979bcc9e2a027ca78b7d204d06d59
hinadome <hinadome@gmail.com>
2023-04-11 17:46:23 -0700

    Create output.tf

---
+++ devops/tf/output.tf
@@ -0,0 +1,7 @@
+output "webapp_first_host" {
+    value = "${linode_instance.cfe-pyapp.0.label}: ${linode_instance.cfe-pyapp.0.ip_address}"
+}
+
+output "webapp_hosts" {
+    value = [for host in linode_instance.cfe-pyapp.*: "${host.label}: ${host.ip_address}"]
+}
